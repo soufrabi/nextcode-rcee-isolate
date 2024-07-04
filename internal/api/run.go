32aad9c6d0b1126ca47709e97f99ca9dee36ce66
Anirban Dey <anirbandey@soufrabi.com>
2024-07-04 22:39:41 +0530

    Add more parameters to RunRequest and format file

--- internal/api/run.go
+++ internal/api/run.go
@@ -1,15 +1,19 @@
 package api
 
 type RunRequest struct {
-    SourceCode string `json:"sourceCode"`
-    Stdin      string `json:"stdin"`
-    CpuTimeLimit uint16 `json:"cpuTimeLimit"`
-    MemoryLimit uint16 `json:"memoryLimit"`
+	SourceCode               string `json:"sourceCode"`
+	Stdin                    string `json:"stdin"`
+	CpuTimeLimit             uint64 `json:"cpuTimeLimit"`
+	CpuExtraTime             uint64 `json:"cpuExtraTime"`
+	WallTimeLimit            uint64 `json:"wallTimeLimit"`
+	MaxProcessesAndOrThreads uint64 `json:"maxProcessesAndOrThreads"`
+	MemoryLimit              uint64 `json:"memoryLimit"`
+	StackLimit               uint64 `json:"stackLimit"`
+	MaxFileSize              uint64 `json:"maxFileSize"`
 }
 
 type RunResponse struct {
-    Stdout string `json:"stdout"`
-    Stderr string `json:"stderr"`
-    Status string `json:"status"`
+	Stdout string `json:"stdout"`
+	Stderr string `json:"stderr"`
+	Status string `json:"status"`
 }
-
