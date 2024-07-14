76e849cb894854909a45e1736fdbe602406e51a2
Anirban Dey <anirbandey@soufrabi.com>
2024-07-14 14:39:39 +0530

    Setup log/slog, gin logging level with GO_ENV

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-    "git.soufrabi.com/nextcode/rcee-isolate/internal/web"
+	"git.soufrabi.com/nextcode/rcee-isolate/internal/jobs"
+	"git.soufrabi.com/nextcode/rcee-isolate/internal/web"
+	"github.com/gin-gonic/gin"
+	"log/slog"
+	"os"
 )
 
+func setupLogHandler(goEnv string) {
+
+	var logHandler *slog.JSONHandler
+	if goEnv == "production" {
+		logHandler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})
+		gin.SetMode(gin.ReleaseMode)
+	} else {
+		logHandler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})
+		gin.SetMode(gin.DebugMode)
+	}
+	slog.SetDefault(slog.New(logHandler))
+}
+
 func main() {
-    web.SetupServer()
+	var err error
+	var goEnv string = os.Getenv("GO_ENV")
+
+	setupLogHandler(goEnv)
+
+	err = jobs.InitializeIsolate()
+	if err != nil {
+		os.Exit(1)
+	}
+	web.SetupServer()
 }
