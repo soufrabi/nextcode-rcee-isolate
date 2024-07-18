9d498c3bdda4ead709a541f5fd56b92813506c58
Anirban Dey <anirbandey@soufrabi.com>
2024-07-18 19:13:59 +0530

    Add language JAVASCRIPT_NODEJS_18

--- internal/proglang/lang.go
+++ internal/proglang/lang.go
@@ -9,8 +9,9 @@ type Language struct {
 }
 
 const (
-	PYTHON_CPYTHON_3_12 uint64 = 1
-	RUBY_3_2            uint64 = 2
+	PYTHON_CPYTHON_3_12  uint64 = 1
+	RUBY_3_2             uint64 = 2
+	JAVASCRIPT_NODEJS_18 uint64 = 3
 )
 
 var LangMap = map[uint64]Language{
@@ -25,5 +26,11 @@ var LangMap = map[uint64]Language{
 		InterpreterNeeded: true,
 		FileExtension:     "rb",
 		InterpreterName:   "ruby3.2",
+	},
+	JAVASCRIPT_NODEJS_18: {
+		CompilationNeeded: false,
+		InterpreterNeeded: true,
+		FileExtension:     "js",
+		InterpreterName:   "node",
 	},
 }
